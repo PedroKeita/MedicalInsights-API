681d397ba1f3d4ce577d97061a499cd6f674808b
PedroKeita <pedrolucas300805@gmail.com>
2025-10-18 15:01:44 -0300

    feature: create GET endpoint for patient, listing all and a specific patient by id

--- src/routes/patientRoutes.ts
+++ src/routes/patientRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import  { createPatient }  from '../controllers/patientController.js';
+import  { createPatient, listPatient, getPatientById }  from '../controllers/patientController.js';
 
 const router = Router();
 
@@ -59,5 +59,9 @@ const router = Router();
  *         description: Falta de campos obrigatórios
  */
 router.post('/patients', createPatient);
+
+router.get('/patients', listPatient);
+
+router.get('/patients/:id', getPatientById);
 
 export default router;
